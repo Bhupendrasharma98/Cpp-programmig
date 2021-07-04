5301bba03861d9ab2b6129ca7eb7e48997ab8661
bhupendarsharma98 <84843057+Bhupendrasharma98@users.noreply.github.com>
2021-07-04 18:42:33 +0530

    Add files via upload

---
+++ Electricity Bill.cpp
@@ -0,0 +1,29 @@
+/*Electricity Bill
+Electricity board has decided to charge rupees based on the units consumed by a particular home. If the units consumed is less than or equal to 200, the cost for one unit is 0.5. If the unit is less than or equal to 400, the cost for one unit is 0.65 and Rs.100 extra charge. If the unit is less than or equal to 600, the cost for one unit is 0.80 and Rs.200 extra charge. If the unit is greater than 600 the cost for one unit is 1.25 and Rs.425 extra charge. You need to now calculate the electricity bill based on the units consumed (given input).
+INPUT & OUTPUT FORMAT:
+
+Input consists of one integer.
+
+This corresponds to the units consumed.
+
+The output consists of one integer.
+
+This corresponds to the electricity bill in rupees.*/
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+  int u,a;
+ cin>>u;
+ if(u<=200)
+ a = u*0.5;
+ if(u>200 && u<=400)
+   a = (u*0.65)+100;
+ if(u>400 && u<=600)
+   a = (u*0.80) + 200;
+ if(u>600)
+   a = (u*1.25)+ 425;
+ cout<<"Rs."<<a;
+}
